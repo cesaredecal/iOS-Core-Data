4e20b15ba6a732f7169722d8b6825bf06d87ac9b
Cesare de Cal <contact@cesare.io>
2018-02-24 17:16:01 +0100

    Created Xcode project

---
+++ iOS-Core-Data/iOS-Core-Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Core-Data
+//
+//  Created by Cesare de Cal on 2/24/18.
+//  Copyright © 2018 Cesare Gianfilippo Astianatte de Cal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
